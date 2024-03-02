8dfb9d28f2ee3d4ed2a61ebc70c9759ed96c4c40
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-03-01 21:59:38 -0700

    tweaked shapes.js class method names

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -2,8 +2,9 @@
 const { Triangle, Square, Circle } = require('../lib/shapes');
 
 describe ('Triangle', () => {
-    it('will show up as a "polygon" with the user-specified color background', () => {
+    it('will show up as a "polygon" with the user-specified [english] color background', () => {
         const shape = new Triangle('text');
+        // In this test, we ensure that we can enter a regular/plain english color and it'll populate as is
         shape.textColor = "blue";
         shape.shapeColor = "yellow";
         expect(shape.renderSVG()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg" >
@@ -17,8 +18,9 @@ describe ('Triangle', () => {
 })
 
 describe ('Square', () => {
-    it('will show up as a "rect" with the user-specified color background', () => {
+    it('will show up as a "rect" with the user-specified hexidecimal [#000000] color background', () => {
         const shape = new Square('text');
+        // In this test, we also ensure we can enter a full hexidecimal code (with the hash (#) included) and it'll populate as is
         shape.textColor = "#980521";
         shape.shapeColor = "#850421";
         expect(shape.renderSVG()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg" >
@@ -32,8 +34,9 @@ describe ('Square', () => {
 })
 
 describe ('Circle', () => {
-    it('will show up as a "circle" with the user-specified color background', () => {
+    it('will show up as a "circle" with the user-specified hexidecimal [000000] color background', () => {
         const shape = new Circle('text');
+        // In this test, we also ensure that we can enter just the six digit hexidecimal tihout the hash (#) and it'll add the hash (#)
         shape.textColor = "134983";
         shape.shapeColor = "432542";
         expect(shape.renderSVG()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg" >
